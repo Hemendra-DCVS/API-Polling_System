793ec290c10a9f561b7161ba7d78ac110de791b1
Hemendra-DCVS <hemendrachanti@gmail.com>
2023-10-20 14:31:00 +0530

    api

--- api/controllers/v1/question-controller.js
+++ api/controllers/v1/question-controller.js
@@ -2,41 +2,24 @@ const question = require('../../models/question');// Import the model
 const option = require('../../models/options'); // Import the model
 
 
-// module.exports.questions = async function (req, res) {
-//     try {
-//         // Retrieve questions data from the database
-//         const questions = await question.find().populate('options'); // Populate the 'options' field
-
-//         // Map the results to include only the text and votes from options
-//         const all_questions = questions.map((q) => ({
-//             query: q.query,
-//             options: q.options.map((o) => ({
-//                 text: o.text,
-//                 votes: o.votes
-//             }))
-//         }));
-
-//         // Send the questions data as JSON in the response
-//         res.status(200).json({
-//             message: 'This list contains All Questions',
-//             data: all_questions
-//         });
-//     } catch (error) {
-//         // Handle any errors
-//         console.error('Error retrieving questions data:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// };
-
 module.exports.questions = async function (req, res) {
     try {
-        // Retrieve questions data from the database with the 'options' field populated
-        const questions = await question.find().populate('options');
+        // Retrieve questions data from the database
+        const questions = await question.find().populate('options'); // Populate the 'options' field
+
+        // Map the results to include only the text and votes from options
+        const all_questions = questions.map((q) => ({
+            query: q.query,
+            options: q.options.map((o) => ({
+                text: o.text,
+                votes: o.votes
+            }))
+        }));
 
         // Send the questions data as JSON in the response
         res.status(200).json({
             message: 'This list contains All Questions',
-            data: questions
+            data: all_questions
         });
     } catch (error) {
         // Handle any errors
@@ -45,37 +28,12 @@ module.exports.questions = async function (req, res) {
     }
 };
 
-// module.exports.create = async function (req, res) {
-//     try {
-//         console.log('*******', req.body);
-//         // Create a new question by passing the data from the request body
-//         const { query } = req.body;
-
-//         // Create a new question with the provided query
-//         const newQuestion = await question.create({ query });
-
-//         // Send a success response with the created question
-//         return res.status(201).json({ 
-//             message: 'Question created successfully',
-//             question: newQuestion 
-//         });
-//     } catch (error) {
-//         // Handle any errors
-//         console.error('Error creating question:', error);
-//         return res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// };
 
 module.exports.create = async function (req, res) {
     try {
         console.log('*******', req.body);
         // Create a new question by passing the data from the request body
         const { query } = req.body;
-
-        // Validate the query field
-        if (req.body.length === 0) {
-            throw new Error('Query must not be empty.');
-        }
 
         // Create a new question with the provided query
         const newQuestion = await question.create({ query });
@@ -86,17 +44,43 @@ module.exports.create = async function (req, res) {
             question: newQuestion 
         });
     } catch (error) {
-        console.log(error);
-        // Handle validation errors
-        if (error.name === 'ValidationError') {
-            return res.status(400).json({ error: error.message });
-        }
-
-        // Handle other errors
+        // Handle any errors
         console.error('Error creating question:', error);
         return res.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
+// module.exports.create = async function (req, res) {
+//     try {
+//         console.log('*******', req.body);
+//         // Create a new question by passing the data from the request body
+//         const { query } = req.body;
+
+//         // Validate the query field
+//         if (req.body.length === 0) {
+//             throw new Error('Query must not be empty.');
+//         }
+
+//         // Create a new question with the provided query
+//         const newQuestion = await question.create({ query });
+
+//         // Send a success response with the created question
+//         return res.status(201).json({ 
+//             message: 'Question created successfully',
+//             question: newQuestion 
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         // Handle validation errors
+//         if (error.name === 'ValidationError') {
+//             return res.status(400).json({ error: error.message });
+//         }
+
+//         // Handle other errors
+//         console.error('Error creating question:', error);
+//         return res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// };
 
 module.exports.deleteQuestion = async function (req, res) {
     try {
--- api/models/question.js
+++ api/models/question.js
@@ -3,36 +3,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema; // Add this line to import Schema
 const option = require('./options');
 // Define a new Mongoose schema for tasks
-// const questionSchema = new mongoose.Schema({
-//   //query (required string)
-//   query: {
-//     type: String,
-//     required: true
-//   },
-//   // options referance
-//   options: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'options'
-//   },
-  
-// }, { timestamps: true }); // Enable timestamps for created and updated fields
-
 const questionSchema = new mongoose.Schema({
+  //query (required string)
   query: {
     type: String,
-    required: true,
-    validate: {
-      validator: (v) => v.length > 0,
-      message: 'Query must not be empty.',
-      query: "what is the capital of france"
-    }
-
+    required: true
   },
+  // options referance
   options: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'options'
   },
-}, { timestamps: true });
+  
+}, { timestamps: true }); // Enable timestamps for created and updated fields
+
+// const questionSchema = new mongoose.Schema({
+//   query: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: (v) => v.length > 0,
+//       message: 'Query must not be empty.',
+//       query: "what is the capital of france"
+//     }
+
+//   },
+//   options: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'options'
+//   },
+// }, { timestamps: true });
 
 
 
